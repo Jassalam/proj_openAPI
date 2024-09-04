126c84110eed2cc3b73f748385215947320c4fa6
Jasveer <jasveerk1980@gmail.com>
2024-09-04 16:07:23 +0200

    new open api

---
+++ src/main/java/com/example/newprojapi/NewProjApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.newprojapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewProjApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewProjApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/newprojapi/NewProjApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newprojapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewProjApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
